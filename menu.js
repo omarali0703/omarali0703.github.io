80d00f1c2c4ea40b3326ca1c30065ee5d36ded66
omarali0703 <omar.ali0703@gmail.com>
2017-01-17 20:15:27 +0000

    21

---
+++ menu.js
@@ -0,0 +1,19 @@
+function toggle() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if(!event.taget.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementByClassName("dropdown-content");
+    var i;
+
+    for(i = 0; i< dropdowns.length; i ++) {
+      var openDropdown = dropdowns[i];
+
+      if(openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
